4b7b468799cf634723afe568e9b2f28f1574f1ce
Wilfredo <me@wilfredocasas.com>
2020-06-02 13:23:27 +0200

    created models and routes folders

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
-const { Favors } = require('./models/favorSchema.js')
-
-
-async function createUser(username) {
-  return new User({
-    username,
-    created: Date.now()
-  }).save()
-}
-
+const favors = require("./routes/favors");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,35 +14,12 @@ mongoose
   .then(() => console.log("connected to db"))
   .catch(err => console.log(err));
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/createFavor", async (req, res) => {
-  console.log("let's create a favor", req.body)
-  try {
-    const newFavor = await Favors.create(req.body);
-    res.status(200).json({
-      status: "success",
-      data: newFavor
-    });
-  } catch (err) {
-    res.status(500).json({
-      status: "fail",
-      message: err
-    });
-    console.error(err);
-  }
-});
+app.use("/api/favors", favors);
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
 
-app.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
