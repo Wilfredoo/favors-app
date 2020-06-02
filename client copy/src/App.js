d9edd096978986ed4af4888dcd3a8e42fe6ec10a
Wilfredo <me@wilfredocasas.com>
2020-06-02 11:33:08 +0200

    client copy

---
+++ client copy/src/App.js
@@ -0,0 +1,28 @@
+import React,{useEffect,useState} from 'react';
+import axios from 'axios';
+
+
+const App = () => {
+
+
+    useEffect(() => {
+        axios.get('/api/hello')
+            .then(res => {
+               console.log("res", res)
+            })
+            .catch(err => {
+                console.log("err", err)
+            })
+    }, []);
+    
+    
+
+        return (
+            <div>
+                Loading...
+            </div>
+        );
+    
+};
+
+export default App
